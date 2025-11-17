66bddefd201e0129bbd1e7e5be3815fe9f28535c
Tormod G Hellen <tormod.hellen@gmail.com>
2025-11-17 23:50:38 +0100

    doodle

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,128 @@
 #![feature(f16)]
 #![feature(f128)]
+#![feature(float_algebraic)]
 
-struct Algebraic<T> {
+pub trait AlgebraicFloatTrait {
+    fn algebraic_add(self, rhs: Self) -> Self;
+    fn algebraic_sub(self, rhs: Self) -> Self;
+    fn algebraic_mul(self, rhs: Self) -> Self;
+    fn algebraic_div(self, rhs: Self) -> Self;
+    fn algebraic_rem(self, rhs: Self) -> Self;
+}
+
+impl AlgebraicFloatTrait for Algebraic<f16> {
+    fn algebraic_add(self, rhs: Self) -> Self {
+        Algebraic {
+            value: self.value.algebraic_add(rhs.value),
+        }
+    }
+    fn algebraic_sub(self, rhs: Self) -> Self {
+        Algebraic {
+            value: self.value.algebraic_sub(rhs.value),
+        }
+    }
+    fn algebraic_mul(self, rhs: Self) -> Self {
+        Algebraic {
+            value: self.value.algebraic_mul(rhs.value),
+        }
+    }
+    fn algebraic_div(self, rhs: Self) -> Self {
+        Algebraic {
+            value: self.value.algebraic_div(rhs.value),
+        }
+    }
+    fn algebraic_rem(self, rhs: Self) -> Self {
+        Algebraic {
+            value: self.value.algebraic_rem(rhs.value),
+        }
+    }
+}
+
+impl AlgebraicFloatTrait for Algebraic<f32> {
+    fn algebraic_add(self, rhs: Self) -> Self {
+        Algebraic {
+            value: self.value.algebraic_add(rhs.value),
+        }
+    }
+    fn algebraic_sub(self, rhs: Self) -> Self {
+        Algebraic {
+            value: self.value.algebraic_sub(rhs.value),
+        }
+    }
+    fn algebraic_mul(self, rhs: Self) -> Self {
+        Algebraic {
+            value: self.value.algebraic_mul(rhs.value),
+        }
+    }
+    fn algebraic_div(self, rhs: Self) -> Self {
+        Algebraic {
+            value: self.value.algebraic_div(rhs.value),
+        }
+    }
+    fn algebraic_rem(self, rhs: Self) -> Self {
+        Algebraic {
+            value: self.value.algebraic_rem(rhs.value),
+        }
+    }
+}
+
+impl AlgebraicFloatTrait for Algebraic<f64> {
+    fn algebraic_add(self, rhs: Self) -> Self {
+        Algebraic {
+            value: self.value.algebraic_add(rhs.value),
+        }
+    }
+    fn algebraic_sub(self, rhs: Self) -> Self {
+        Algebraic {
+            value: self.value.algebraic_sub(rhs.value),
+        }
+    }
+    fn algebraic_mul(self, rhs: Self) -> Self {
+        Algebraic {
+            value: self.value.algebraic_mul(rhs.value),
+        }
+    }
+    fn algebraic_div(self, rhs: Self) -> Self {
+        Algebraic {
+            value: self.value.algebraic_div(rhs.value),
+        }
+    }
+    fn algebraic_rem(self, rhs: Self) -> Self {
+        Algebraic {
+            value: self.value.algebraic_rem(rhs.value),
+        }
+    }
+}
+
+impl AlgebraicFloatTrait for Algebraic<f128> {
+    fn algebraic_add(self, rhs: Self) -> Self {
+        Algebraic {
+            value: self.value.algebraic_add(rhs.value),
+        }
+    }
+    fn algebraic_sub(self, rhs: Self) -> Self {
+        Algebraic {
+            value: self.value.algebraic_sub(rhs.value),
+        }
+    }
+    fn algebraic_mul(self, rhs: Self) -> Self {
+        Algebraic {
+            value: self.value.algebraic_mul(rhs.value),
+        }
+    }
+    fn algebraic_div(self, rhs: Self) -> Self {
+        Algebraic {
+            value: self.value.algebraic_div(rhs.value),
+        }
+    }
+    fn algebraic_rem(self, rhs: Self) -> Self {
+        Algebraic {
+            value: self.value.algebraic_rem(rhs.value),
+        }
+    }
+}
+
+pub struct Algebraic<T> {
     value: T,
 }
 
