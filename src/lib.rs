8a4ef67baa129ef2a565bc47f822e37c8813757e
Tormod G Hellen <tormod.hellen@gmail.com>
2025-11-17 23:55:21 +0100

    doodle

--- src/lib.rs
+++ src/lib.rs
@@ -126,44 +126,44 @@ pub struct Algebraic<T> {
     value: T,
 }
 
-impl From<f16> for Algebraic<f16> {
-    fn from(value: f16) -> Self {
-        Algebraic { value }
+impl From<&f16> for Algebraic<f16> {
+    fn from(value: &f16) -> Self {
+        Algebraic { value: *value }
     }
 }
 
-impl From<f32> for Algebraic<f32> {
-    fn from(value: f32) -> Self {
-        Algebraic { value }
+impl From<&f32> for Algebraic<f32> {
+    fn from(value: &f32) -> Self {
+        Algebraic { value: *value }
     }
 }
-impl From<f64> for Algebraic<f64> {
-    fn from(value: f64) -> Self {
-        Algebraic { value }
+impl From<&f64> for Algebraic<f64> {
+    fn from(value: &f64) -> Self {
+        Algebraic { value: *value }
     }
 }
-impl From<f128> for Algebraic<f128> {
-    fn from(value: f128) -> Self {
-        Algebraic { value }
+impl From<&f128> for Algebraic<f128> {
+    fn from(value: &f128) -> Self {
+        Algebraic { value: *value }
     }
 }
 
-impl Into<f16> for Algebraic<f16> {
+impl Into<f16> for &Algebraic<f16> {
     fn into(self) -> f16 {
         self.value
     }
 }
-impl Into<f32> for Algebraic<f32> {
+impl Into<f32> for &Algebraic<f32> {
     fn into(self) -> f32 {
         self.value
     }
 }
-impl Into<f64> for Algebraic<f64> {
+impl Into<f64> for &Algebraic<f64> {
     fn into(self) -> f64 {
         self.value
     }
 }
-impl Into<f128> for Algebraic<f128> {
+impl Into<f128> for &Algebraic<f128> {
     fn into(self) -> f128 {
         self.value
     }
